ece351a1e4fa198f42877d931178c584a8a34f06
Pushkar Shembekar <pushembekar@gmail.com>
2018-07-21 14:51:17 -0400

    Builder pattern

---
+++ FacetedBuilder/FacetedBuilder/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace FacetedBuilder
+{
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, " +
+                $"{nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder // facade
+    {
+        // reference object; cannot be a struct
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+
+        public Person GetPerson()
+        {
+            return this.person;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyname)
+        {
+            person.CompanyName = companyname;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            var person = pb
+                .Works.At("OST").AsA("Engineer").Earning(123000)
+                .Lives.At("1550 Main St").WithPostcode("22222").In("Siver Spring")
+                .GetPerson();
+
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ FluentBuilder/FluentBuilder/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentBuilder
+{
+    class Program
+    {
+        public class HtmlElement
+        {
+            public string Name, Text;
+            public List<HtmlElement> Elements = new List<HtmlElement>();
+            private const int indentsize = 2;
+
+            public HtmlElement()
+            {
+
+            }
+
+            public HtmlElement(string name, string text)
+            {
+                Name = name;
+                Text = text;
+            }
+
+            private string ToStringImpl(int indent)
+            {
+                var sb = new StringBuilder();
+                var i = new string(' ', indentsize * indent);
+                sb.AppendLine($"{i}<{Name}>");
+
+                if (!string.IsNullOrWhiteSpace(Text))
+                {
+                    sb.Append(new string(' ', indentsize * (indent + 1)));
+                    sb.AppendLine(Text);
+                }
+
+                foreach (var e in Elements)
+                {
+                    sb.Append(e.ToStringImpl(indent + 1));
+                }
+                sb.AppendLine($"{i}</{Name}>");
+
+                return sb.ToString();
+            }
+
+            public override string ToString()
+            {
+                return ToStringImpl(0);
+            }
+        }
+
+        public class HtmlBuilder
+        {
+            private readonly string rootName;
+            HtmlElement root = new HtmlElement();
+
+            public HtmlBuilder(string rootName)
+            {
+                this.rootName = rootName;
+                root.Name = rootName;
+            }
+
+            public HtmlBuilder AddChild(string childName, string childText)
+            {
+                var e = new HtmlElement(childName, childText);
+                root.Elements.Add(e);
+                return this;
+            }
+
+            public override string ToString()
+            {
+                return root.ToString();
+            }
+
+            public void Clear()
+            {
+                root = new HtmlElement { Name = rootName };
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Hello").AddChild("li", "World");
+
+            Console.WriteLine(builder);
+        }
+    }
+}
